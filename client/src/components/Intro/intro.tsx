39e2d54eb743e8605735bc8404cb960f81956031
ali morgan <alimorgaan@gmail.com>
2023-09-15 02:00:48 +0300

    ui updates

--- client/src/components/Intro/intro.tsx
+++ client/src/components/Intro/intro.tsx
@@ -1,108 +1,116 @@
-import { useNavigate } from 'react-router-dom';
-import { Button } from '../ui/button';
-import { useTheme } from '../theme/theme-provider.js';
-import '../Project/Project.css';
-import Editor, { loader } from '@monaco-editor/react';
-import Markdown from '../Project/MarkDown.jsx';
-import { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
+import { useTheme } from "../theme/theme-provider.js";
+import "../Project/Project.css";
+import Editor, { loader } from "@monaco-editor/react";
+import Markdown from "../Project/MarkDown.jsx";
+import { useEffect, useState } from "react";
 
 const Intro = () => {
   const navigate = useNavigate();
   useEffect(() => {
-    if(localStorage.getItem('token')) {
-      navigate('/project');
+    if (localStorage.getItem("token")) {
+      navigate("/project");
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const theme = useTheme();
   const [loading, setLoading] = useState(true);
-  const [value, setValue] = useState('# Welcome to Markdown Playground!');
+  const [value, setValue] = useState("# Welcome to Markdown Playground!");
   loader.init().then((monaco) => {
-    monaco.editor.defineTheme('myTheme', {
-      base: 'vs-dark',
+    monaco.editor.defineTheme("myTheme", {
+      base: "vs-dark",
       inherit: true,
       rules: [],
       colors: {
-        'editor.background': '#030712',
-        'editor.lineHighlightBackground': '#00000000',
-        'editor.lineHighlightBorder': '#00000000',
+        "editor.background": "#030712",
+        "editor.lineHighlightBackground": "#00000000",
+        "editor.lineHighlightBorder": "#00000000",
       },
     });
   });
-  let editorTheme = '';
-  if (theme.theme === 'dark') {
-    editorTheme = 'vs-dark';
-  } else if (theme.theme === 'light') {
-    editorTheme = 'vs-light';
-  } else if (theme.theme === 'system') {
-    editorTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-      ? 'vs-dark'
-      : 'vs-light';
+  let editorTheme = "";
+  if (theme.theme === "dark") {
+    editorTheme = "vs-dark";
+  } else if (theme.theme === "light") {
+    editorTheme = "vs-light";
+  } else if (theme.theme === "system") {
+    editorTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "vs-dark"
+      : "vs-light";
   }
 
   return (
     <>
-      <div className='container flex flex-col items-center justify-center py-20'>
-        <h1 className='text-center text-4xl md:text-5xl lg:text-6xl font-bold tracking-normal leading-normal  '>
+      <div className="container flex flex-col items-center justify-center py-20">
+        <h1 className="text-center text-4xl md:text-5xl lg:text-6xl font-bold tracking-normal leading-normal  ">
           Your Friendly Markdown Playground for Effortless Creativity!
         </h1>
-        <p className='mt-6 text-center   text-gray-400'></p>
-        <div className='flex'>
+        <p className="mt-6 text-center   text-gray-400"></p>
+        <div className="flex">
           <Button
-            variant='secondary'
-            className=' text-xl  mr-4'
-            size='lg'
+            variant="secondary"
+            className=" text-xl  mr-4"
+            size="lg"
             onClick={() => {
-              navigate('/login');
+              navigate("/login");
             }}
           >
             Login
           </Button>
           <Button
-            size='lg'
-            className='text-xl'
+            size="lg"
+            className="text-xl"
             onClick={() => {
-              navigate('/signUp');
+              navigate("/signUp");
             }}
           >
             Signup
           </Button>
         </div>
-        <h3 className='text-center text-4xl  font-bold tracking-normal leading-normal my-10 '>
-          Try it out!
-        </h3>
-        <div className='flex w-full'>
-          <Editor
-            value={value}
-            onMount={() => setLoading(false)}
-            onChange={(changedValue) =>
-              setValue(changedValue ? changedValue : value)
-            }
-            height='100% !important'
-            width='50%'
-            defaultLanguage='markdown'
-            options={{
-              theme: editorTheme,
-              wordWrap: 'on',
-              minimap: { enabled: false },
-              showUnused: false,
-              folding: false,
-              lineNumbersMinChars: 3,
-              fontSize: 16,
-              scrollBeyondLastLine: false,
-              automaticLayout: true,
-              scrollbar: {
-                vertical: 'visible',
-                horizontal: 'visible',
-                verticalScrollbarSize: 10,
-                horizontalScrollbarSize: 10,
-              },
-            }}
-          />
+        <div
+          className={` mt-12 hidden rounded-lg border-[20px]   ${
+            theme.theme === "dark"
+              ? "bg-neutral-900 border-neutral-900"
+              : "  border-neutral-50"
+          } shadow-[0px_-24px_300px_0px_rgba(57,140,203,0.15)] transition hover:shadow-[0px_-24px_150px_0px_rgba(57,140,203,0.3)] md:block`}
+        >
+          <h3 className="text-center text-4xl  font-bold tracking-normal leading-normal my-5 ">
+            Try it out!
+          </h3>
+          <div className="flex w-full">
+            <Editor
+              value={value}
+              onMount={() => setLoading(false)}
+              onChange={(changedValue) =>
+                setValue(changedValue ? changedValue : value)
+              }
+              height="100% !important"
+              width="50%"
+              defaultLanguage="markdown"
+              options={{
+                theme: editorTheme,
+                wordWrap: "on",
+                minimap: { enabled: false },
+                showUnused: false,
+                folding: false,
+                lineNumbersMinChars: 3,
+                fontSize: 16,
+                scrollBeyondLastLine: false,
+                automaticLayout: true,
+                scrollbar: {
+                  vertical: "visible",
+                  horizontal: "visible",
+                  verticalScrollbarSize: 10,
+                  horizontalScrollbarSize: 10,
+                },
+              }}
+            />
 
-          <div className='markdown border-solid border-y border-r'>
-            <Markdown value={value} loading={loading} />
+            <div className="markdown border-solid border-y border-r">
+              <Markdown value={value} loading={loading} />
+            </div>
           </div>
         </div>
       </div>
--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,7 @@
-import { cn } from '@/lib/utils';
-import { Project } from '../ProjectsSideBar/types';
-import { Button } from '@/components/ui/button';
-import { useNavigate } from 'react-router-dom';
-import { Pencil, Trash } from 'lucide-react';
+import { Project } from "../ProjectsSideBar/types";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+import { Pencil, Trash } from "lucide-react";
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
   projects: Project[];
@@ -14,7 +13,6 @@ interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export function Sidebar({
-  className,
   projects,
   selectedProjectId,
   handleCreateProject,
@@ -24,51 +22,51 @@ export function Sidebar({
   const navigate = useNavigate();
 
   return (
-    <div className={cn('pb-12', className)}>
-      <div className='space-y-4 py-4'>
-        <div className='px-3 py-2'>
+    <div className={"pb-12"}>
+      <div className="space-y-4 py-4">
+        <div className="px-3 py-2">
           <Button
-            className='w-full  text-center justify-center mb-5'
+            className="w-full  text-center justify-center mb-5"
             onClick={() => {
               handleCreateProject();
             }}
           >
             Create Project
           </Button>
-          <div className='space-y-1'>
+          <div className="space-y-1">
             {projects.map((project) => (
               <Button
                 key={project.project.id}
                 variant={`${
                   project.project.id.toString() === selectedProjectId
-                    ? 'secondary'
-                    : 'ghost'
+                    ? "secondary"
+                    : "ghost"
                 }`}
-                className='w-full justify-between px-2'
+                className="w-full justify-between px-2"
                 onClick={() => {
-                  navigate('/project/' + project.project.id);
+                  navigate("/project/" + project.project.id);
                 }}
               >
                 {project.project.name}
                 {project.project.id.toString() === selectedProjectId && (
                   <div>
                     <Button
-                      className=' ml-auto px-2 py-1'
-                      variant='ghost'
+                      className=" ml-auto px-2 py-1"
+                      variant="ghost"
                       onClick={() => {
                         handleRenameProject(project.project.id);
                       }}
                     >
-                      <Pencil className='h-4 w-4'/>
+                      <Pencil className="h-4 w-4" />
                     </Button>
                     <Button
-                      className=' ml-auto px-2 py-1'
-                      variant='ghost'
+                      className=" ml-auto px-2 py-1"
+                      variant="ghost"
                       onClick={() => {
                         handleDeleteProject(project.project.id);
                       }}
                     >
-                      <Trash className='h-4 w-4' />
+                      <Trash className="h-4 w-4" />
                     </Button>
                   </div>
                 )}
